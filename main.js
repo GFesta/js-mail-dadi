8bde8fc9a3d77594c02ecab5817aa579533f3e29
Giuseppe <festagiuseppe89@gmail.com>
2021-02-23 16:19:48 +0100

    Compito Email completato

--- main.js
+++ main.js
@@ -2,26 +2,20 @@
 //controlla che sia nella lista di chi può accedere,
 //stampa un messaggio appropriato sull’esito del controllo.
 
+var emailList = ["gmail","hotmail","yahoo", "outlook"];
+console.log(emailList);
+
 var inserisciEmail = prompt ("Inserisci la tua email");
-var email = ["gmail","hotmail","yahoo","icloud", "outlook"];
-email [0] = "gmail";
-email [1] = "hotmail";
-email [2] = "icloud";
-email [3] = "outlook";
+console.log(inserisciEmail);
 
-if (inserisciEmail = email); {
-    alert ("Email corretta");
-    document.getElementById('benvenuto').innerHTML = "Benvenuto ";
-} 
-//else if (inserisciEmail != email); {
-    //alert ("Devi inserire un indirizzo mail corretto");
-//}
-//document.getElementById('non-abilitato').innerHTML = "Devi inserire un indirizzo mail corretto";
-
-
-
-
-
+//verifica email hce hanno accesso
+if (inserisciEmail == (emailList[0]) || inserisciEmail == (emailList[1]) || inserisciEmail == (emailList[2]) || inserisciEmail == (emailList[3])) {
+    console.log("Benvenuto");
+    alert("Benvenuto");
+}
+else {
+    alert("Non sei abilitato all'accesso");
+}
 
 //Generare un numero random da 1  a 6, sia per il giocatore sia per il computer
 //Stabilire il vincitore, in base a chi fa il punteggio più alto.
